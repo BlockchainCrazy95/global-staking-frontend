8f8cd4710ccd98ee6affa2f7871299af72b656ad
BlockchainCrazy95 <happyworld0905@gmail.com>
2022-09-16 12:08:32 +0300

    v1

--- staking-frontend/src/components/Sidebar/NavContent.jsx
+++ staking-frontend/src/components/Sidebar/NavContent.jsx
@@ -14,26 +14,11 @@ import { ReactComponent as WrapIcon } from "../../assets/icons/wrap.svg";
 import { ReactComponent as BridgeIcon } from "../../assets/icons/bridge.svg";
 import { ReactComponent as ArrowUpIcon } from "../../assets/icons/arrow-up.svg";
 import { trim, shorten } from "../../helpers";
-import { useAddress, useWeb3Context } from "src/hooks/web3Context";
-import useBonds from "../../hooks/Bonds";
+import { useAddress, useWeb3Context } from "src/hooks/web3Context"
 import { Paper, Link, Box, Typography as Typograp, SvgIcon } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
 import "./sidebar.scss";
 import LogoImg from '../../assets/icons/olympus-nav-header.png'
-import Bondimg from '../../assets/ohm/bond@2x.png'
-import Bondimg2 from '../../assets/ohm/bond2@2x.png'
-import Dashboardimg from '../../assets/ohm/dashboard@2x.png'
-import Dashboardimg2 from '../../assets/ohm/dashboard2@2x.png'
-import Docsimg from '../../assets/ohm/docs@2x.png'
-import Docsimg2 from '../../assets/ohm/docs2@2x.png'
-import Stakeimg from '../../assets/ohm/stake@2x.png'
-import Stakeimg2 from '../../assets/ohm/stake2@2x.png'
-
-import Togetherimg from '../../assets/ohm/33-1@2x.png'
-import Togetherimg2 from '../../assets/ohm/33@2x.png'
-
-import NFTimg from '../../assets/ohm/nft-1@2x.png'
-import NFTimg2 from '../../assets/ohm/nft@2x.png'
 
 
 
@@ -42,27 +27,11 @@ import NFTimg2 from '../../assets/ohm/nft@2x.png'
 function NavContent() {
   const [isActive] = useState();
   const address = useAddress();
-  const { bonds } = useBonds();
   const { chainID } = useWeb3Context();
 
   const checkPage = useCallback((match, location, page) => {
     const currentPath = location.pathname.replace("/", "");
-    if (currentPath.indexOf("dashboard") >= 0 && page === "dashboard") {
-      return true;
-    }
     if (currentPath.indexOf("stake") >= 0 && page === "stake") {
-      return true;
-    }
-    if (currentPath.indexOf("Presale") >= 0 && page === "Presale") {
-      return true;
-    }
-    if (currentPath.indexOf("calculator") >= 0 && page === "calculator") {
-      return true;
-    }
-    if (currentPath.indexOf("nft") >= 0 && page === "nft") {
-      return true;
-    }
-    if ((currentPath.indexOf("bonds") >= 0 || currentPath.indexOf("choose_bond") >= 0) && page === "bonds") {
       return true;
     }
     return false;
@@ -94,106 +63,6 @@ function NavContent() {
               </div>
             )}
           </Box>
-
-          <div className="dapp-menu-links">
-            <div className="dapp-nav" id="navbarNav">
-              <Link
-                component={NavLink}
-                id="dash-nav"
-                to="/dashboard"
-                isActive={(match, location) => {
-                  return checkPage(match, location, "dashboard");
-                }}
-                className={`button-dapp-menu ${isActive ? "active" : ""}`}
-              >
-                <Typography variant="h6">
-                  <SvgIcon color="primary" component={DashboardIcon} />
-                  Dashboard
-                </Typography>
-              </Link>
-
-              <Link
-                component={NavLink}
-                id="presale-nav"
-                to="/presale"
-                isActive={(match, location) => {
-                  return checkPage(match, location, "Presale");
-                }}
-                className={`button-dapp-menu ${isActive ? "active" : ""}`}
-              >
-                <Typography variant="h6">
-                  <SvgIcon color="primary" component={WrapIcon} />
-                  Presale
-                </Typography>
-              </Link>
-
-              <Link
-                component={NavLink}
-                id="bond-nav"
-                to="/bonds"
-                isActive={(match, location) => {
-                  return checkPage(match, location, "bonds");
-                }}
-                className={`button-dapp-menu ${isActive ? "active" : ""}`}
-              >
-                <Typography variant="h6">
-                  <SvgIcon color="primary" component={BondIcon} />
-                  Bond
-                </Typography>
-              </Link>
-
-              <div className="dapp-menu-data discounts">
-                <div className="bond-discounts">
-                  <Typography variant="body2">Bond discounts</Typography>
-                  {bonds.map((bond, i) => (
-                    <Link component={NavLink} to={`/bonds/${bond.name}`} key={i} className={"bond"}>
-                      {!bond.bondDiscount ? (
-                        <Skeleton variant="text" width={"150px"} />
-                      ) : (
-                        <Typography variant="body2">
-                          {bond.displayName}
-                          <span className="bond-pair-roi">
-                            {bond.bondDiscount && trim(bond.bondDiscount * 100, 2)}%
-                          </span>
-                        </Typography>
-                      )}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-
-
-              <Link
-                component={NavLink}
-                id="stake-nav"
-                to="/stake"
-                isActive={(match, location) => {
-                  return checkPage(match, location, "stake");
-                }}
-              // className={`button-dapp-menu ${isActive ? "active" : ""}`}
-              >
-                <Typography variant="h6" className="fxCenter">
-                  <SvgIcon color="primary" component={StakeIcon} />
-                  Stake
-                </Typography>
-              </Link>
-
-              <Link
-                component={NavLink}
-                id="calculator-nav"
-                to="/calculator"
-                isActive={(match, location) => {
-                  return checkPage(match, location, "Calculator");
-                }}
-                className={`button-dapp-menu ${isActive ? "active" : ""}`}
-              >
-                <Typography variant="h6">
-                  <SvgIcon color="primary" component={PoolTogetherIcon} />
-                  Calculator
-                </Typography>
-              </Link>
-            </div>
-          </div>
         </div>
         <Box className="dapp-menu-bottom" display="flex" justifyContent="space-between" flexDirection="column">
           <div className="dapp-menu-external-links">
